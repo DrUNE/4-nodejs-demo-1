ae2c690eb236ae7d822943014a0272dbfb0e8a25
Andrey Litvinov <drune.email@gmail.com>
2026-09-10 18:24:14 +0300

    second

---
+++ 3_calc/index.js
@@ -0,0 +1,18 @@
+const { add } = require("./add");
+const { multiply } = require("./multiply");
+const { subtract } = require("./subtract");
+const { divide } = require("./divide");
+
+const [, , a, b, operation] = process.argv;
+
+const operations = {
+  add,
+  multiply,
+  subtract,
+  divide,
+};
+
+const op = operations[String(operation).toLowerCase()];
+if (op) {
+  console.log(op(parseInt(a, 10), parseInt(b, 10)));
+}
