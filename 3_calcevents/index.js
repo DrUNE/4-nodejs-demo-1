ba7b7e4298ad072dd8e4c9b29004060c5de94aff
Andrey Litvinov <drune.email@gmail.com>
2026-09-10 18:45:07 +0300

    evented

---
+++ 3_calcevents/index.js
@@ -0,0 +1,21 @@
+const { EventEmitter } = require("node:events");
+const { add } = require("./add");
+const { multiply } = require("./multiply");
+const { subtract } = require("./subtract");
+const { divide } = require("./divide");
+
+const [, , a, b, operation] = process.argv;
+
+const ee = new EventEmitter();
+const operations = {
+  add,
+  multiply,
+  subtract,
+  divide,
+};
+Object.keys(operations).forEach((operation) =>
+  ee.on(operation, (a, b) => ee.emit("result", operations[operation](a, b))),
+);
+ee.on("result", (result) => console.log(result));
+
+ee.emit(String(operation).toLowerCase(), parseInt(a, 10), parseInt(b, 10));
