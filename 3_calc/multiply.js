b6733614707462a5dbef9db4fcbf3c94af223630
Andrey Litvinov <drune.email@gmail.com>
2026-09-13 11:30:08 +0300

    tsdoc comment

--- 3_calc/multiply.js
+++ 3_calc/multiply.js
@@ -1,5 +1,13 @@
+/**
+ * @this {void}
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
 module.exports = {
-  multiply(a, b) {
-    return a * b;
-  },
+  multiply,
 };
