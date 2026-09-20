3c9ee310bdbc7b70581ae8e65f2ae3220124b324
Andrey Litvinov <drune.email@gmail.com>
2026-09-20 16:34:33 +0300

    DZ-7

---
+++ 7-timer-notifications/index.js
@@ -0,0 +1,32 @@
+import nn from 'node-notifier';
+const [, , ...args] = process.argv;
+
+function durationToMs(str) {
+  const regex = /(\d+)\s*(h|m|s)/gi;
+  let totalMs = 0;
+  let match;
+
+  while ((match = regex.exec(str)) !== null) {
+    const value = Number(match[1]);
+    const unit = match[2].toLowerCase();
+
+    switch (unit) {
+      case 'h':
+        totalMs += value * 60 * 60 * 1000;
+        break;
+      case 'm':
+        totalMs += value * 60 * 1000;
+        break;
+      case 's':
+        totalMs += value * 1000;
+        break;
+    }
+  }
+
+  return totalMs;
+}
+
+setTimeout(
+  () => nn.notify({ title: 'Timer notification', message: 'Время истекло!' }),
+  durationToMs(args.join(' ')),
+);
